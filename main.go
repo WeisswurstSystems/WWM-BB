492b148d3f9c0c137e32cca2c0c1686f9044510c
Fabian Holtkötter <fabianholtkoetter@gmail.com>
2017-05-13 02:57:42 +0200

    set port to PORT env variable if available

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
     "encoding/json"
     "log"
+    "os"
     "net/http"
  
     "github.com/gorilla/mux"
@@ -65,5 +66,10 @@ func main() {
     router.HandleFunc("/people/{id}", GetPersonEndpoint).Methods("GET")
     router.HandleFunc("/people/{id}", CreatePersonEndpoint).Methods("POST")
     router.HandleFunc("/people/{id}", DeletePersonEndpoint).Methods("DELETE")
-    log.Fatal(http.ListenAndServe(":8080", router))
+    port := os.Getenv("PORT")
+    if len(port) == 0 {
+        log.Fatal(http.ListenAndServe(":8080", router))
+    } else {
+        log.Fatal(http.ListenAndServe(":" + port, router))
+    }
 }
